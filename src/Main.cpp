a83aad64f8561fdc645086398b1ab539ee56b53a
Vinay Komaravolu <vinay.komaravolu@mail.utoronto.ca>
2020-03-02 01:31:29 -0500

    added opengl support

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,6 @@
-#include <vulkan.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <vulkan.h>
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/vec4.hpp>
@@ -8,26 +9,44 @@
 #include <iostream>
 
 int main() {
-    glfwInit();
-
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
+    // Vulkan Test
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
     std::cout << extensionCount << " extensions supported" << std::endl;
 
+    // GLM Test
     glm::mat4 matrix;
     glm::vec4 vec;
     auto test = matrix * vec;
 
+    // GLFW Test
+    if (!glfwInit()) {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Visual Construct", nullptr, nullptr);
+
+    glfwMakeContextCurrent(window);
+
+    // Glad Test
+    if (!gladLoadGL()) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    } 
+
+    // OpenGL Test  
+   glClearColor(0.15f, 0.6f, 0.4f, 1.0f);
+
     while(!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     glfwDestroyWindow(window);
-
     glfwTerminate();
 
     return 0;
