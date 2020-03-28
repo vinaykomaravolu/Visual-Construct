729f4cf05274ab1ddded0619d4e26a751237365a
Vinay Komaravolu <vinay.komaravolu@mail.utoronto.ca>
2020-03-28 19:34:23 -0400

    Added bullet3 to project

---
+++ dep/bullet3/src/BulletCollision/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/redzo/Downloads/bullet3-2.89/bullet3-2.89/src/BulletCollision
+# Build directory: C:/Users/redzo/Downloads/bullet3-2.89/bullet3-2.89/src/BulletCollision
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- src/Main.cpp
+++ src/Main.cpp
@@ -9,14 +9,14 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
-
-
+#include <btBulletDynamicsCommon.h>
 #include <iostream>
 
 int main() {
     // Vulkan Test
     uint32_t extensionCount = 0;
-    if (vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr) == VK_SUCCESS) {
+    if (vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr) == VK_SUCCESS) 
+    {
         VC_LOG_CORE_INFO("Vulkan Test: Pass");
     }
 
@@ -27,11 +27,13 @@ int main() {
     VC_LOG_CORE_INFO("GLM Test: Pass");
 
     // GLFW Test
-    if (!glfwInit()) {
+    if (!glfwInit()) 
+    {
         std::cout << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
-    else {
+    else 
+    {
         VC_LOG_CORE_INFO("GLFW Test: Pass");
     }
 
@@ -41,11 +43,13 @@ int main() {
     glfwSwapInterval(1); // Enable vsync
 
     // Glad Test
-    if (!gladLoadGL()) {
+    if (!gladLoadGL()) 
+    {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-    else {
+    else 
+    {
         VC_LOG_CORE_INFO("GLAD Test: Pass");
     }
 
@@ -66,6 +70,138 @@ int main() {
 
     // OpenGL Test  
     VC_LOG_CORE_INFO("OpenGL Test: Pass");
+
+    // Bullet Physics Test
+    btDefaultCollisionConfiguration* collisionConfiguration = new
+        btDefaultCollisionConfiguration();
+
+    btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
+
+    btBroadphaseInterface* overlappingPairCache = new btDbvtBroadphase(); // btAxis3Sweep
+
+    btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
+
+    btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher,
+        overlappingPairCache, solver, collisionConfiguration);
+
+    dynamicsWorld->setGravity(btVector3(0, -10, 0));
+
+    btAlignedObjectArray<btCollisionShape*> collisionShapes;
+
+    {
+        btCollisionShape* groundShape = new btBoxShape(btVector3(btScalar(50.), btScalar(50.), btScalar(50.)));
+
+        collisionShapes.push_back(groundShape);
+
+        btTransform groundTransform;
+        groundTransform.setIdentity();
+        groundTransform.setOrigin(btVector3(0, -56, 0));
+
+        btScalar mass(0.);
+
+        //rigidbody is dynamic if and only if mass is non zero, otherwise static
+        bool isDynamic = (mass != 0.f);
+
+        btVector3 localInertia(0, 0, 0);
+        if (isDynamic)
+            groundShape->calculateLocalInertia(mass, localInertia);
+
+        //using motionstate is optional, it provides interpolation capabilities, and only synchronizes 'active' objects
+        btDefaultMotionState* myMotionState = new btDefaultMotionState(groundTransform);
+        btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, groundShape, localInertia);
+        btRigidBody* body = new btRigidBody(rbInfo);
+
+        //add the body to the dynamics world
+        dynamicsWorld->addRigidBody(body);
+    }
+
+    {
+        //create a dynamic rigidbody
+
+        btCollisionShape* colShape = new btSphereShape(btScalar(1.));
+        collisionShapes.push_back(colShape);
+
+        /// Create Dynamic Objects
+        btTransform startTransform;
+        startTransform.setIdentity();
+        startTransform.setOrigin(btVector3(2, 10, 0));
+
+        btScalar mass(1.f);
+
+        //rigidbody is dynamic if and only if mass is non zero, otherwise static
+        bool isDynamic = (mass != 0.f);
+
+        btVector3 localInertia(0, 0, 0);
+        if (isDynamic)
+            colShape->calculateLocalInertia(mass, localInertia);
+
+        //using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
+        btDefaultMotionState* myMotionState = new btDefaultMotionState(startTransform);
+        btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, colShape, localInertia);
+        btRigidBody* body = new btRigidBody(rbInfo);
+
+        dynamicsWorld->addRigidBody(body);
+    }
+
+
+    for (int i = 0; i < 100; i++) 
+    {
+        dynamicsWorld->stepSimulation(1.f / 60.f,10);
+        for (int j = dynamicsWorld->getNumCollisionObjects() - 1; j > 0; j--)
+        {
+            btCollisionObject* obj = dynamicsWorld->getCollisionObjectArray()[j];
+            btRigidBody* body = btRigidBody::upcast(obj);
+            btTransform trans;
+            if (body && body->getMotionState()) 
+            {
+                body->getMotionState()->getWorldTransform(trans);
+            }
+            else
+            {
+                trans = obj->getWorldTransform();
+            }
+            printf("world pos object %d = %f,%f,%f\n", j, float(trans.getOrigin().getX()), float(trans.getOrigin().getY()), float(trans.getOrigin().getZ()));
+
+        }
+    }
+    // remove the rigidbodies from the dynamics world and delete them
+    for (int i = dynamicsWorld->getNumCollisionObjects() - 1; i >= 0; i--)
+    {
+        btCollisionObject* obj = dynamicsWorld->getCollisionObjectArray()[i];
+        btRigidBody* body = btRigidBody::upcast(obj);
+        if (body && body->getMotionState())
+        {
+            delete body->getMotionState();
+        }
+        dynamicsWorld->removeCollisionObject(obj);
+        delete obj;
+    }
+
+    //delete collision shapes
+    for (int j = 0; j < collisionShapes.size(); j++)
+    {
+        btCollisionShape* shape = collisionShapes[j];
+        collisionShapes[j] = 0;
+        delete shape;
+    }
+
+    //delete dynamics world
+    delete dynamicsWorld;
+
+    //delete solver
+    delete solver;
+
+    //delete broadphase
+    delete overlappingPairCache;
+
+    //delete dispatcher
+    delete dispatcher;
+
+    delete collisionConfiguration;
+
+    //next line is optional: it will be cleared by the destructor when the array goes out of scope
+    collisionShapes.clear();
+    VC_LOG_CORE_INFO("Bullet3 Test: Pass");
 
     // Main loop
     while (!glfwWindowShouldClose(window))
