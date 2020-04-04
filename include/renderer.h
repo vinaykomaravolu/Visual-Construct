88e5f90bdfc16f9ea5a3904c4646c38778a98370
Vinay Komaravolu <vinay.komaravolu@mail.utoronto.ca>
2020-04-04 19:32:44 -0400

    Fixed deconstructor symbol not found

--- include/renderer.h
+++ include/renderer.h
@@ -4,7 +4,6 @@ public:
 	virtual void render() = 0;
 	virtual void render(int height, int width) = 0;
 	virtual void getRender(void** renderedTextureID) = 0;
-	virtual ~Renderer();
 protected:
 	int baseHeight = 1080;
 	int baseWidth = 1920;
--- src/Main.cpp
+++ src/Main.cpp
@@ -2,6 +2,7 @@
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
+#include <imgui_impl_vulkan.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <vulkan.h>
